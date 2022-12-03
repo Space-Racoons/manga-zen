7cd62e7c3c4c88d9889b986d58a734f36bca0655
WesleyVitor <wesleydemorais@outlook.com.br>
2022-12-03 17:22:10 -0300

    refactor: Revertendo para o boilerplate iniciaial

--- apps/users/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from apps.users.views import LoginAPIView
-
-urlpatterns = [
-    path('login/',LoginAPIView.as_view(), name="login" ),
-]
--- manga_zen/settings.py
+++ manga_zen/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'apps.users.apps.UsersConfig',
+    
 ]
 
 MIDDLEWARE = [
