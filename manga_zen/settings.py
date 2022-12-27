787319c3732ed46f9e21ab122e527c3ed533cbda
Felipe David <felipedavid.huh@gmail.com>
2022-12-27 14:19:40 -0300

    feat: Patching up models

--- manga_zen/settings.py
+++ manga_zen/settings.py
@@ -81,6 +81,8 @@ DATABASES = {
     }
 }
 
+# Letting Django know about the new User model
+AUTH_USER_MODEL = 'zen.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
--- zen/models.py
+++ zen/models.py
@@ -1,33 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-
-class User(models.Model):
-    username = models.CharField(max_length=30)
-    email = models.CharField(max_length=200)
-    # TODO(felipe): Checar melhor maneira de lidar com passwords em django. Quase
-    # certeza que isso não é recomendado.
-    hashed_password = models.CharField(max_length=300)
-    image = models.CharField(max_length=100)
+class User(AbstractUser):
+    image_url = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.username
 
-class Genre(models.Model):
+
+class Status(models.Model):
     name = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Manga(models.Model):
-    # NOTE(felipe): I don't think there is a reason for mangas and users have a relation
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    genre = models.ForeignKey(Genre, on_delete=models.DO_NOTHING)
-    author = models.CharField(max_length=200)
-    # NOTE(felipe): What is this exactly? is stuff like ongoing/finished/yato? If so,
-    # we should probably do the same we did with the attribute genre.
-    status = models.CharField(max_length=50)
+    status = models.ForeignKey(Status, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=300)
+    author = models.CharField(max_length=300)
     views = models.IntegerField()
     description = models.CharField(max_length=3000)
     url = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Genre(models.Model):
+    manga = models.ForeignKey(Manga, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Chapter(models.Model):
@@ -36,11 +46,16 @@ class Chapter(models.Model):
     image_url = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f'{self.ch_number}, {self.manga.title[:10]}'
+
 
 class Comment(models.Model):
-    # NOTE(felipe): If the user is deleted, sure we want to delete all his comments?
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
     manga = models.ForeignKey(Manga, on_delete=models.CASCADE)
     content = models.CharField(max_length=2000)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user}, {self.content[:10]}'
